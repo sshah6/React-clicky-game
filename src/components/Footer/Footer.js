44e2c5899ad095541715d76f96bde69cc7234d78
Said M Shah <saidmshah93@gmail.com>
2018-10-22 17:03:47 -0700

    adding and checking if works

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Footer.css";
+
+// set the footer to a copyright string
+const Footer = props => (
+  <div className="footer">
+    <p class="text-muted text-center">Images featuring <i>Disney</i> Images are downloaded, Thanks from whoever made these</p>
+  </div>
+);
+
+export default Footer;
